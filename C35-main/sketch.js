9fd8f25f90981f90c8b5c8bb79256d5236f11de3
Ikhos <73320135+Ikhos@users.noreply.github.com>
2021-06-24 15:19:57 -0500

    Add files via upload

---
+++ C35-main/sketch.js
@@ -0,0 +1,41 @@
+var balloonT;
+
+function setup() {
+  createCanvas(800,400);
+  database = firebase.database();
+  balloonT = new Balloon();
+  
+}
+
+function draw() {
+  
+  background(255,255,255);
+  text("Press Down Arrow To Move Balloon", 100, 100);
+  
+  if(keyIsDown(DOWN_ARROW)) {
+    balloonT.y++;
+    balloonT.getPosY();
+    balloonT.updatePosY(balloonT.y);
+  }
+  
+  if(keyIsDown(UP_ARROW)) {
+    balloonT.y--;
+    balloonT.getPosY();
+    balloonT.updatePosY(balloonT.y);
+  }
+  
+  if(keyIsDown(LEFT_ARROW)) {
+    balloonT.x--;
+    balloonT.getPosX();
+    balloonT.updatePosX(balloonT.x);
+  }
+  
+  if(keyIsDown(RIGHT_ARROW)) {
+    balloonT.x++;
+    balloonT.getPosX();
+    balloonT.updatePosX(balloonT.x);
+  }
+
+  balloonT.display();
+  drawSprites();
+}
